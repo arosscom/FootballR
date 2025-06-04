50245303b60111c383404447b45f8094ff8e4eb0
A. Ross Morton <arossco@ntlworld.com>
2025-06-04 15:55:04 +0100

    Initial Commit

---
+++ FootballR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FootballR
+//
+//  Created by Ross Morton on 04/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FootballR/FootballRApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballRApp.swift
+//  FootballR
+//
+//  Created by Ross Morton on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
